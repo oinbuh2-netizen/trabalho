8a28ac1112c42dbfa773b0f96fd3d3800c00063b
oinbuh2-netizen <oinbuh2@gmail.com>
2025-10-14 15:28:28 -0300

    Add files via upload

---
+++ Conecta-Social-Projeto-main/Conecta-Social-Projeto-main/Trabalho da escola/conecta-social_projeto/README.md
@@ -0,0 +1,5 @@
+===============================================================================================================
+**Rodar no terminal**
+Vai precisar do **pip install flask**
+Depois vá para o arquivo **app.py** execute e no termina tera um Running on http://(Ai o numero que aparecer),clique com o CTRL + CLICK do mause e vc acessara o site
+===============================================================================================================
---
+++ Conecta-Social-Projeto-main/Conecta-Social-Projeto-main/Trabalho da escola/conecta-social_projeto/static/localizacao.js
@@ -0,0 +1,50 @@
+function buscarLocaisProximos(esporte) {
+	if (esporte === 'futebol') {
+		window.open('https://www.google.com/maps/search/futebol+são+joose+do+rio+preto/@-20.7947097,-49.3716367,13.04z/data=!5m1!1e1?entry=ttu&g_ep=EgoyMDI1MTAwNi4wIKXMDSoASAFQAw%3D%3D', '_blank');
+		return;
+	}
+	if (esporte === 'futsal') {
+		window.open('https://www.google.com/maps/search/futesal+s%C3%A3o+joose+do+rio+preto/@-20.7945394,-49.3716368,13z/data=!3m1!4b1!5m1!1e1?entry=ttu&g_ep=EgoyMDI1MTAwOC4wIKXMDSoASAFQAw%3D%3D', '_blank');
+		return;
+	}
+	if (esporte === 'futevolei') {
+		window.open('https://www.google.com/maps/search/futevolei+s%C3%A3o+joose+do+rio+preto/@-20.7941987,-49.371637,13z/data=!3m1!4b1!5m1!1e1?entry=ttu&g_ep=EgoyMDI1MTAwOC4wIKXMDSoASAFQAw%3D%3D', '_blank');
+		return;
+	}
+	if (esporte === 'handebol') {
+		window.open('https://www.google.com/maps/search/handebol+s%C3%A3o+joose+do+rio+preto/@-20.7937139,-49.4952541,11z/data=!3m1!4b1!5m1!1e1?entry=ttu&g_ep=EgoyMDI1MTAwOC4wIKXMDSoASAFQAw%3D%3D', '_blank');
+		return;
+	}
+	if (esporte === 'volei_de_areia') {
+		window.open('https://www.google.com/maps/search/volei+de+areia+s%C3%A3o+joose+do+rio+preto/@-20.7937139,-49.4952541,11z/data=!5m1!1e1?entry=ttu&g_ep=EgoyMDI1MTAwOC4wIKXMDSoASAFQAw%3D%3D', '_blank');
+		return;
+	}
+	if (esporte === 'volei') {
+		window.open('https://www.google.com/maps/search/volei+são+joose+do+rio+preto/@-20.7946999,-49.3718084,13z/data=!3m1!4b1!5m1!1e1?entry=ttu&g_ep=EgoyMDI1MTAwNi4wIKXMDSoASAFQAw%3D%3D', '_blank');
+		return;
+	}
+	if (esporte === 'basquete') {
+		window.open('https://www.google.com/maps/search/Basquete+são+jose+do+rio+preto/@-20.793051,-49.3976703,13z/data=!5m1!1e1?entry=ttu&g_ep=EgoyMDI1MTAwNi4wIKXMDSoASAFQAw%3D%3D', '_blank');
+		return;
+	}
+	if (esporte === 'rugby') {
+		window.open('https://www.google.com/maps/search/rugby+são+jose+do+rio+preto/@-20.8159734,-49.385687,14z/data=!5m1!1e1?entry=ttu&g_ep=EgoyMDI1MTAwNi4wIKXMDSoASAFQAw%3D%3D', '_blank');
+		return;
+	}
+	if (!navigator.geolocation) {
+		alert('Geolocalização não suportada pelo navegador.');
+		return;
+	}
+	navigator.geolocation.getCurrentPosition(function(pos) {
+		const lat = pos.coords.latitude;
+		const lon = pos.coords.longitude;
+		let tipo = '';
+		if (esporte === 'basquete') {
+			tipo = 'quadra de basquete';
+		}
+		const url = `https://www.google.com/maps/search/${encodeURIComponent(tipo)}+perto+de+${lat},${lon}`;
+		window.open(url, '_blank');
+	}, function() {
+		alert('Não foi possível obter sua localização.');
+	});
+}
